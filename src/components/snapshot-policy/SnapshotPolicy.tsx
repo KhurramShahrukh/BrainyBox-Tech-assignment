565bbd0ce70c8e950e939fc1064f95caddb33f5f
Khurram Shahrukh <khurramshahrukh18@gmail.com>
2025-05-01 20:09:17 +0500

    updated snapshot policy screen

--- src/components/snapshot-policy/SnapshotPolicy.tsx
+++ src/components/snapshot-policy/SnapshotPolicy.tsx
@@ -23,6 +23,19 @@ const SnapshotPolicy
                 </div>
                 <br />
                 <PolicyScheduleForm />
+                <br />
+                <div className="text-textColor2 text-cmd ">Snapshot Locking</div>
+                <div className="text-textColor2 ">Locked snapshots cannot be deleted before the deletion schedule expires. For this feature to be available, snapshots must  be set to automatically delete.</div>
+                <input type="checkbox" className="cursor-pointer" />
+                <div className="inline-block mt-2 ml-2 text-textColor2 text-cmd">Enable locked snapshots</div>
+                <br />
+                <br />
+                <br />
+                <input type="checkbox" className="cursor-pointer" />
+                <div className="inline-block ml-2 text-textColor2 text-cmd">Enable policy</div>
+                <br />
+                <button className="cursor-pointer px-4 py-2 mt-2 text-white rounded bg-button1 shadow-[inset_0px_1px_0px_0px_var(--color-shadow1)] text-cmd">Save Policy</button>
+                <button className="mx-8 my-4 cursor-pointer text-button2 text-cmd">Cancel</button>
             </div>
         )
     }
